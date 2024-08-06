41e8e4abb008054a14370f970212d7065453f2a1
William222372 <waiyanoo72@gmail.com>
2024-08-06 12:08:33 +0800

    Update js_string.js

--- js_string.js
+++ js_string.js
@@ -2,3 +2,8 @@ let str="Hello Js";
 let strHeading=document.querySelector("#str");
 strHeading.innerHTML=str;
 
+function FindElementByIndex(){
+    let char_at_ind=document.querySelector("#char_at_ind").value;
+    let ind =str.charAt(char_at_ind);
+    document.querySelector("#r1").innerHTML="Element that we found "+char_at_ind +"is => "+ ele ;
+}
